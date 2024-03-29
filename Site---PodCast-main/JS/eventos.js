4c872543d9ad172b5980432465915ce07ec02f9f
Gabriel-S07 <148344477+Gabriel-S07@users.noreply.github.com>
2024-03-29 19:31:58 -0300

    Next Return

--- Site---PodCast-main/JS/eventos.js
+++ Site---PodCast-main/JS/eventos.js
@@ -24,22 +24,22 @@ const tituloMobile = document.querySelector(".seprarTextoTelaCheia .titulo")
 
 let tituloClicado = "Kyan"
 let codigoClicado = "168"
+let epAtual = "1"
 
 for(let i = 0; i < listaEps.length; i++)
 {
     let epClicado = listaEps[i]
     epClicado.addEventListener("click", function(){
-        let idInput = epClicado.getAttribute("for")
-        let input = document.querySelector(`#${idInput}`)
-        tituloClicado = input.getAttribute("title")
-        codigoClicado = input.getAttribute("ep")
+        tituloClicado = epClicado.getAttribute("title")
+        codigoClicado = epClicado.getAttribute("ep")
+        epAtual = epClicado.getAttribute("cd")
 
         console.log(tituloClicado);
-        console.log(codigoClicado);
+        console.log(epAtual);
 
-        imgPrincipal.src = `Thumbs/${tituloClicado}-PDPH.jpg`
-        imgControlesMobile.src = `Thumbs/${tituloClicado}-PDPH.jpg`
-        imgMobile.src = `Thumbs/${tituloClicado}-PDPH.jpg`
+        imgPrincipal.src = `Thumbs/${tituloClicado}-${epAtual}-PDPH.jpg`
+        imgControlesMobile.src = `Thumbs/${tituloClicado}-${epAtual}-PDPH.jpg`
+        imgMobile.src = `Thumbs/${tituloClicado}-${epAtual}-PDPH.jpg`
 
         tituloVideoClicado.textContent = `PodPah - ${tituloClicado} - EP: #${codigoClicado}`
         tituloVideoClicadoMobile.textContent = `PodPah - ${tituloClicado} - EP: #${codigoClicado}`
@@ -47,3 +47,35 @@ for(let i = 0; i < listaEps.length; i++)
         tituloMobile.textContent = `PodPah - ${tituloClicado} - EP: #${codigoClicado}`
     })
 }
+
+const botaoProximo = document.querySelector("#proximo")
+botaoProximo.addEventListener("click", function(){
+    epAtual++
+    if(epAtual == 8)
+    {
+        epAtual = 1
+    }
+
+    let proximoEp = listaEps[epAtual - 1]
+    proximoEp = proximoEp.getAttribute("title")
+    imgPrincipal.src = `Thumbs/${proximoEp}-${epAtual}-PDPH.jpg`
+    imgControlesMobile.src = `Thumbs/${proximoEp}-${epAtual}-PDPH.jpg`
+    imgMobile.src = `Thumbs/${proximoEp}-${epAtual}-PDPH.jpg`
+})
+
+const botaoAnterior = document.querySelector("#anterior")
+botaoAnterior.addEventListener("click", function(){
+    epAtual--
+    console.log(epAtual);
+    if(epAtual == 0)
+    {
+        epAtual = 7
+    }
+    console.log(epAtual);
+    let epAnterior = listaEps[epAtual - 1]
+    console.log(epAnterior);
+    epAnterior = epAnterior.getAttribute("title")
+    imgPrincipal.src = `Thumbs/${epAnterior}-${epAtual}-PDPH.jpg`
+    imgControlesMobile.src = `Thumbs/${epAnterior}-${epAtual}-PDPH.jpg`
+    imgMobile.src = `Thumbs/${epAnterior}-${epAtual}-PDPH.jpg`
+})
